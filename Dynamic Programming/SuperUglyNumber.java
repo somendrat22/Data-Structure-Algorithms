0a7c36d9e0ad7d8518280c8b1f54bf3e64241258
somendra tiwari <somendrat22@outlook.com>
2022-10-05 00:37:50 +0530

    super ugly number updated

--- Dynamic Programming/SuperUglyNumber.java
+++ Dynamic Programming/SuperUglyNumber.java
@@ -1,7 +1,8 @@
 package Dynamic Programming;
 
 public class SuperUglyNumber {
-    long [] dp = new long[n];
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        long [] dp = new long[n];
         long [] primeIdx = new long[primes.length];
         dp[0] = 1;
         for(int i = 1; i<n; i++){
