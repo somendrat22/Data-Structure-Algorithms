5b7c0e3c9c22a48bc7c2e51c0ce0575e09884d17
somendra tiwari <somendrat22@outlook.com>
2022-10-05 00:37:05 +0530

    supper ugly number dp solution O(n*k) complexity

---
+++ Dynamic Programming/SuperUglyNumber.java
@@ -0,0 +1,23 @@
+package Dynamic Programming;
+
+public class SuperUglyNumber {
+    long [] dp = new long[n];
+        long [] primeIdx = new long[primes.length];
+        dp[0] = 1;
+        for(int i = 1; i<n; i++){
+            long min = (long)Integer.MAX_VALUE;
+            for(int j = 0; j<primes.length; j++){
+                min = Math.min(min,(long)(primes[j]*dp[(int)(primeIdx[j])]));
+            }
+            for(int j = 0; j<primes.length; j++){
+                if(min == primes[j]*dp[(int)(primeIdx[j])]){
+                    primeIdx[j]++;
+                }
+            }
+            dp[i] = min;
+        }
+        
+
+        return (int)dp[n-1];
+    }
+}
